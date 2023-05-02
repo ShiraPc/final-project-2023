4d836c5dbcde31ed9eddf433e41753a76d3762f0
shiraShr <shirasharabani@gmail.com>
2023-05-02 10:05:09 +0300

    first commit

---
+++ routes/routes.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+Injectable()
+export class RoutesService {
+    tryFunc(){
+        let a:Array<Array<number>>;
+        b:[]=[1,2,3];
+        this.getShortestRoute(a);
+    }
+    getShortestRoute(allFars:Array<Array<number>>){
+        
+    }
+}
+
